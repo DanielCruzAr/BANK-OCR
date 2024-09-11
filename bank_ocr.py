1283b3d32972be19a5de336cad7fd649128db20f
DanielCruzAr <daniel_cruzar@outlook.com>
2024-09-11 11:11:18 -0600

    ocr class

--- bank_ocr.py
+++ bank_ocr.py
@@ -19,14 +19,18 @@ class BankOCR:
             
             " _ |_||_|": "8",
             
-            " _|_| _|": "9"
+            " _ |_| _|": "9"
         }
         self.path = path
+        self.blocks = self.get_blocks()
+        self.numbers = []
+        self.parse_numbers()
         
     def get_blocks(self):
+        print(f"leyendo archivo {self.path}...")
         with open(self.path, "r") as file:
             lines = file.readlines()
-            lines = [line.rstrip() for line in lines]
+            lines = [line.replace('\n', '') for line in lines]
             blocks = [lines[i:i+4] for i in range(0, len(lines), 4)]
         return blocks
     
@@ -35,5 +39,28 @@ class BankOCR:
         number = "".join([self.digit_map.get(digit, "?") for digit in digits])
         return number
     
+    def parse_numbers(self):
+        print("transformando numeros...")
+        for block in self.blocks:
+            number = self.get_number(block)
+            self.numbers.append(number)
+    
     def checksum(self, number):
+        if len(number) != 9:
+            return False
         return sum([int(number[i]) * (9 - i) for i in range(9)]) % 11 == 0
+    
+    def write_output(self, path):
+        with open(path, "w") as file:
+            for number in self.numbers:
+                print(f"validando {number}...", end=" ")
+                if "?" in number:
+                    print("ILL")
+                    file.write(f"{number} ILL\n")
+                elif not self.checksum(number):
+                    print("ERR")
+                    file.write(f"{number} ERR\n")
+                else:
+                    print("OK")
+                    file.write(f"{number} OK\n")
+        print(f"resultados guardados en {path}")
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from bank_ocr import BankOCR
+
 def main():
-    pass
+    input_file = "inputs/input.txt"
+    output_file = "outputs/output.txt"
+    
+    ocr = BankOCR(input_file)
+    ocr.write_output(output_file)
 
 if __name__ == '__main__':
     main()
